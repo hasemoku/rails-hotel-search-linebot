442d7accacbf7e7806a895e7ffef4301a4ef7758
hasemoku <kei5_family@icloud.com>
2021-06-12 23:27:33 +0900

    楽天API続き

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -38,6 +38,7 @@ class LineBotController < ApplicationController
         'applicationId' => ENV['RAKUTEN_APPID'],
         'hits' => 5,
         'responseType' => 'small',
+        'datumType' => 1,
         'formatVersion' => 2
       }
       response = http_client.get(url, query)
